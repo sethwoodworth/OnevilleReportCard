3742d69892b465c7a38d26e20a0a4f6b8d529516
evanburchard <evan.burchard@gmail.com>
2011-03-31 02:40:53 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReportCard::Application.config.secret_token = 'bb818e8eefd57ccaf61ddf85f14eb4a2eabb17e270a2b4477c34d84b497e1639f37ff3748a33504912d0bd362c34f967768354b3c210cc64ae98372228e4e3ef'
