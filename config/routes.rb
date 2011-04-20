03cb44320d4cfeb53fcd925033fd735d31c18d95
evanburchard <evan.burchard@gmail.com>
2011-04-20 18:02:58 -0400

    login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ReportCard::Application.routes.draw do
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -46,6 +48,7 @@ ReportCard::Application.routes.draw do
   #     resources :products
   #   end
 
+devise_for :users
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
  root :to => "home#intro"
